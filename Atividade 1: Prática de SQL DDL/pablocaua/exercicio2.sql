3e67f5ccc86c8c3b38dce9186659988540a78fbb
kauagoes00 <Kauagoes10@icloud.com>
2025-09-25 20:49:56 -0300

    Create exercicio2.sql

---
+++ Atividade 1: Prática de SQL DDL/pablocaua/exercicio2.sql
@@ -0,0 +1,15 @@
+# DDl - CREATE, ALTER< DROP
+
+# CREATE (DATABASE OU (TABLE)
+
+CREATE DATABASE loja_roupas;
+
+USE loja_roupas;
+
+CREATE TABLE produtos (
+id_produto INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50) NOT NULL,
+descricao VARCHAR(255),
+preco DECIMAL(10,2) NOT NULL,
+estoque INT NOT NULL DEFAULT 0
+);
